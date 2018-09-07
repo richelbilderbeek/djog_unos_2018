1ce2995acaa5170ad83b346b2d4c43720eef9531
richelbilderbeek <richel@richelbilderbeek.nl>
2018-09-07 15:40:09 +0200

    Program does something

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
-#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-  ::sf::RectangleShape shape(::sf::Vector2f(100.0,250.0));
-  if (shape.getSize().x < 000'050) return 1;
+  sf::RenderWindow window(sf::VideoMode(800, 600), "sfml_unos_2018");
+
+  double angle{0.0};
+
+  // run the program as long as the window is open
+  while (window.isOpen())
+  {
+    // check all the window's events that were triggered since the last iteration of the loop
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      // "close requested" event: we close the window
+      if (event.type == sf::Event::Closed)
+      window.close();
+    }
+
+    // clear the window with black color
+    window.clear(sf::Color::Black);
+
+    // draw everything here...
+    sf::RectangleShape shape(sf::Vector2f(12.34, 56.78));
+    shape.setPosition(100, 100);
+    shape.setRotation(angle);
+    angle += 0.1;
+    window.draw(shape);
+
+    // end the current frame
+    window.display();
+  }
 }
