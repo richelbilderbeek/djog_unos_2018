efc9ceee1f1b8c28c73546c6c33e29ac471e83e5
richelbilderbeek <richel@richelbilderbeek.nl>
2018-09-21 13:01:00 +0200

    Prepare #87

--- tile.cpp
+++ tile.cpp
@@ -53,4 +53,38 @@ void test_tile()
     }
   }
   #endif // FIX_ISSUE_85_TEST_TILE
+
+
+  //#define FIX_ISSUE_87_SET_TILE_SPEED
+  #ifdef FIX_ISSUE_87_SET_TILE_SPEED
+  //A tile starts from standstill
+  {
+    const tile t(0.0, 0.0, 10.0, 10.0, tile_type::grassland);
+    assert(t.get_dx() == 0.0);
+    assert(t.get_dy() == 0.0);
+  }
+  //Speed is set correctly
+  {
+    tile t(0.0, 0.0, 10.0, 10.0, tile_type::grassland);
+    const double dx{12.34};
+    const double dy{56.78};
+    t.set_dx(dx);
+    t.set_dy(dx);
+    assert(t.get_dx() == dx);
+    assert(t.get_dy() == dy);
+  }
+  //Tile responds to its speed
+  {
+    tile t(0.0, 0.0, 10.0, 10.0, tile_type::grassland);
+    const double dx{12.34};
+    const double dy{56.78};
+    t.set_dx(dx);
+    t.set_dy(dx);
+    assert(t.get_x() != dx);
+    assert(t.get_y() != dy);
+    t.move();
+    assert(t.get_x() == dx);
+    assert(t.get_y() == dy);
+  }
+  #endif // FIX_ISSUE_87_SET_TILE_SPEED
 }
