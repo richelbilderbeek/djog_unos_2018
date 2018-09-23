0aca79176c45f09624c0716e9843c1b64e01584b
richelbilderbeek <richel@richelbilderbeek.nl>
2018-09-23 11:09:34 +0200

    Retrieved lost files

---
+++ game.h
@@ -0,0 +1,38 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <vector>
+
+#include "tile.h"
+
+class game
+{
+
+public:
+  ///Constructor
+  game();
+
+  ///Read all tiles
+  const auto& get_tiles() const noexcept { return m_tiles; }
+
+  int get_score() const noexcept { return m_score; }
+
+  void change_score_by(int delta_score);
+
+private:
+
+  ///Timer, physics, bullets moving, etc.
+  ///Everything except user input.
+  void process_events();
+
+  std::vector<tile> m_tiles;
+
+  int m_n_tick = 0;
+
+  int m_score;
+};
+
+///Test the game class
+void test_game();
+
+#endif // GAME_H
