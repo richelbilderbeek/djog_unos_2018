da6c9be912b36a1ed7d15420466cff6efa2ecdb0
EllyJet <ellesjet@gmail.com>
2018-10-12 22:01:26 +0200

    renamed about_screen to sfml_about_screen

--- djog_unos_2018.pri
+++ djog_unos_2018.pri
@@ -1,6 +1,5 @@
 
 SOURCES += \
-    $$PWD/about_screen.cpp \
     $$PWD/agent.cpp \
     $$PWD/agent_type.cpp \
     $$PWD/game.cpp \
@@ -8,10 +7,10 @@ SOURCES += \
     $$PWD/sfml_game_delegate.cpp \
     $$PWD/sfml_resources.cpp \
     $$PWD/tile.cpp \
-    $$PWD/tile_type.cpp
+    $$PWD/tile_type.cpp \
+    $$PWD/sfml_about_screen.cpp
 
 HEADERS += \
-    $$PWD/about_screen.h \
     $$PWD/agent.h \
     $$PWD/agent_type.h \
     $$PWD/game.h \
@@ -19,7 +18,8 @@ HEADERS += \
     $$PWD/sfml_game_delegate.h \
     $$PWD/sfml_resources.h \
     $$PWD/tile.h \
-    $$PWD/tile_type.h
+    $$PWD/tile_type.h \
+    $$PWD/sfml_about_screen.h
 
 RESOURCES += \
     $$PWD/djog_unos_2018.qrc
--- sfml_about_screen.cpp
+++ sfml_about_screen.cpp
@@ -1,10 +1,10 @@
-#include "about_screen.h"
+#include "sfml_about_screen.h"
 //#include "Text.hpp"
 //#include <SFML/Graphics.hpp>
 #include <QFile>
 #include <cassert>
 
-about_screen::about_screen(const int window_width, const int window_height)
+sfml_about_screen::sfml_about_screen(const int window_width, const int window_height)
     : aboutwindow(sf::VideoMode(static_cast<unsigned int>(window_width),
                                 static_cast<unsigned int>(window_height)),
                   "About") {
@@ -17,9 +17,9 @@ about_screen::about_screen(const int window_width, const int window_height)
   }
 }
 
-void about_screen::close() { aboutwindow.close(); }
+void sfml_about_screen::close() { aboutwindow.close(); }
 
-void about_screen::display() {
+void sfml_about_screen::display() {
   while (aboutwindow.isOpen()) {
     sf::Event event;
     while (aboutwindow.pollEvent(event)) { //!OCLINT indeed an empty while statement
@@ -30,17 +30,17 @@ void about_screen::display() {
     assert(QFile::exists("OpenSans.ttf"));
     font.loadFromFile("OpenSans.ttf");
     aboutwindow.clear(sf::Color::Green); // Clear the window with black color
-    about_screen::abouttext.setFont(font);
-    about_screen::abouttext.setString("About lalala..");
-    about_screen::abouttext.setCharacterSize(24); // in pixels, not points!
+    sfml_about_screen::abouttext.setFont(font);
+    sfml_about_screen::abouttext.setString("About lalala..");
+    sfml_about_screen::abouttext.setCharacterSize(24); // in pixels, not points!
 
     // set the color
     #if(SFML_VERSION_MINOR > 1)
-    about_screen::abouttext.setFillColor(sf::Color::Red);
+    sfml_about_screen::abouttext.setFillColor(sf::Color::Red);
     #else
-    about_screen::abouttext.setColor(sf::Color::Red);
+    sfml_about_screen::abouttext.setColor(sf::Color::Red);
     #endif
-    aboutwindow.draw(about_screen::abouttext);
+    aboutwindow.draw(sfml_about_screen::abouttext);
     aboutwindow.display();
   }
 }
