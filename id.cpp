c327d038cd1da12c4dac9ecd25492c2de58f8a1d
richelbilderbeek <richel@richelbilderbeek.nl>
2018-11-22 13:09:00 +0100

    Comment more OCLints

--- id.cpp
+++ id.cpp
@@ -2,7 +2,7 @@
 
 int new_id() noexcept
 {
-  static int old_id = 0; //!OCLINT use static here
+  static int old_id = 0; //!OCLINT accepted idiom to use static here
   ++old_id;
   return old_id;
 }
