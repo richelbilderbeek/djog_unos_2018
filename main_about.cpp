169c178d59ca9b109ba89d50e959d3822c9d55e7
richelbilderbeek <richel@richelbilderbeek.nl>
2018-11-23 15:25:30 +0100

    'main_about' compiles

--- main_about.cpp
+++ main_about.cpp
@@ -3,15 +3,14 @@
 #include "agent.h"
 #include "sfml_game.h"
 #include "sfml_game_delegate.h"
-#include "about_screen.h"
+#include "sfml_about_screen.h"
 #include <QFile>
 #include <SFML/Graphics.hpp>
 #include <cassert>
 
 int main()
 {
-   about_screen a(1000, 600);
-   a.display();
-   a.close();
+  sfml_about_screen a(1000, 600);
+  a.exec();
 }
 
