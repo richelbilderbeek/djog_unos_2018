2e862703c94a204359d792250be011918b17d95d
Modanung <frode@lindeijer.nl>
2018-11-23 20:57:34 +0100

    Fixed #209

--- sfml_game.h
+++ sfml_game.h
@@ -109,6 +109,10 @@ public:
   void set_agent_sprite(const agent& a, sf::Sprite& sprite);
 
 private:
+
+  void display_tile(tile& t);
+  void display_agent(agent& a);
+
   /// Background music file object
   sf::Music &m_background_music;
 
