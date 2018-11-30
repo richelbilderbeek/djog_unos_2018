850469934f10cfcdb06fc02d32cbf8fa50b428ee
richelbilderbeek <richel@richelbilderbeek.nl>
2018-11-30 19:32:55 +0100

    No unused local variable, thanks OCLint

--- game.cpp
+++ game.cpp
@@ -198,9 +198,6 @@ void test_game() //!OCLINT a testing function may be long
     assert(count_n_tiles(g) == 1);
     assert(collect_tile_types(g)[0] == tile_type::mountains);
   }
-  {
-    game g;
-  }
 }
 
 game load(const std::string &filename) {
