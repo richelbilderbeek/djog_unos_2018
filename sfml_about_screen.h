1b76ca450fa81eed2a0c953777914f9dbfe3253f
richelbilderbeek <richel@richelbilderbeek.nl>
2018-12-02 18:20:02 +0100

    Compiles locally

--- sfml_about_screen.h
+++ sfml_about_screen.h
@@ -4,13 +4,13 @@
 #include <SFML/Graphics.hpp>
 
 class sfml_about_screen {
-private:
+public:
   sfml_about_screen(const int window_width = 1000, const int window_height = 600);
   void close();
   void exec();
- public:
+private:
   sf::RenderWindow aboutwindow;
   sf::Text abouttext;
-
+};
 
 #endif // SFML_ABOUT_SCREEN_H
