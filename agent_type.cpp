aba92752003a8300b7278f96cd703a3e8a3508b9
richelbilderbeek <richel@richelbilderbeek.nl>
2018-12-07 18:33:54 +0100

    Fixed #188. Fixed #204. Fixed #224

--- agent_type.cpp
+++ agent_type.cpp
@@ -35,9 +35,6 @@ void test_agent_type() //!OCLINT testing functions may be long
     #endif
   }
   {
-    //Uncomment if you want to run this test
-    #define FIX_ISSUE_188
-    #ifdef FIX_ISSUE_188
     const std::vector<agent_type> v = collect_all_agent_types();
     for (const agent_type t : v)
     {
@@ -45,25 +42,18 @@ void test_agent_type() //!OCLINT testing functions may be long
       const agent_type u = to_agent(s);
       assert(t == u);
     }
-    #endif // FIX_ISSUE_188
   }
   {
-    #define FIX_ISSUE_224
-    #ifdef FIX_ISSUE_224
     static_assert(agent_type::cow != agent_type::bacterium, "bacterium must exist");
-    #endif
   }
   //Collect all agent_types
   {
-    //#define FIX_ISSUE_204
-    #ifdef FIX_ISSUE_204
     const std::vector<agent_type> v = collect_all_agent_types();
     assert(std::count(std::begin(v), std::end(v), agent_type::cow) == 1);
     assert(std::count(std::begin(v), std::end(v), agent_type::crocodile) == 1);
     assert(std::count(std::begin(v), std::end(v), agent_type::fish) == 1);
     assert(std::count(std::begin(v), std::end(v), agent_type::grass) == 1);
     assert(std::count(std::begin(v), std::end(v), agent_type::none) == 1);
-    #endif
   }
 }
 
