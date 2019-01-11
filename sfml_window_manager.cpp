85c7a7ea626dd43a97f863c547f06406201dfd98
Joshua260403 <joshuavwaardenberg@gmail.com>
2019-01-11 21:25:05 +0100

    window thingy

--- sfml_window_manager.cpp
+++ sfml_window_manager.cpp
@@ -1,6 +1,17 @@
 #include "sfml_window_manager.h"
+#include <cassert>
+
+sfml_window_manager *sfml_window_manager::m_instance = nullptr; //!OCLINT static accepted singleton
 
 sfml_window_manager::sfml_window_manager()
 {
 
+}
+
+sfml_window_manager &sfml_window_manager::get() {
+  if (!m_instance) {
+    m_instance = new sfml_window_manager();
+  }
+  assert(m_instance);
+  return *m_instance;
 }
--- sfml_window_manager.h
+++ sfml_window_manager.h
@@ -1,11 +1,22 @@
 #ifndef SFML_WINDOW_MANAGER_H
 #define SFML_WINDOW_MANAGER_H
 
+#include <SFML/Graphics.hpp>
 
 class sfml_window_manager
 {
 public:
+
+  sfml_window_manager& get();
+
+private:
+
   sfml_window_manager();
+
+  static sfml_window_manager *m_instance;
+
+  sf::RenderWindow m_window;
+
 };
 
 #endif // SFML_WINDOW_MANAGER_H
