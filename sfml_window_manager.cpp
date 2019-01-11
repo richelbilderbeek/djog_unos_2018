0ce8fe748fb0f3b8b6423f7f1a9899340eb1ed12
Joshua260403 <joshuavwaardenberg@gmail.com>
2019-01-11 19:30:20 +0100

    start window manager

---
+++ sfml_window_manager.cpp
@@ -0,0 +1,6 @@
+#include "sfml_window_manager.h"
+
+sfml_window_manager::sfml_window_manager()
+{
+
+}
---
+++ sfml_window_manager.h
@@ -0,0 +1,11 @@
+#ifndef SFML_WINDOW_MANAGER_H
+#define SFML_WINDOW_MANAGER_H
+
+
+class sfml_window_manager
+{
+public:
+  sfml_window_manager();
+};
+
+#endif // SFML_WINDOW_MANAGER_H
