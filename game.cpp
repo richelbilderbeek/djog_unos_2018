5a187a09ed27842aefb2811ebc625bec55bc2e45
janderkkotlarski <j.d.kotlarski@gmx.com>
2019-01-17 19:54:27 +0100

    I think this is a more elegant solution to the problem.

--- game.cpp
+++ game.cpp
@@ -74,20 +74,24 @@ void game::merge_tiles() { //!OCLINT must simplify
       assert(j >=0);
       assert(j < static_cast<int>(m_tiles.size()));
       const tile& other_tile = m_tiles[j];
-      if (!have_same_position(focal_tile, other_tile)) return;
-      const tile_type merged_type = get_merge_type(
-        focal_tile.get_type(),
-        other_tile.get_type()
-      );
-      //focal tile becomes merged type
-      focal_tile.set_type(merged_type);
-      //other tile is swapped to the back, then deleted
-      m_tiles[j] = m_tiles.back();
-      m_tiles.pop_back();
-      //change the selected tile
-      m_selected.clear();
-      assert(m_selected.empty());
-      return; //!OCLINT early return the only good option?
+      if (have_same_position(focal_tile, other_tile))
+      // If the tiles are on the same exact position, then do the following.
+      // The inverse of the previous.
+      {
+        const tile_type merged_type = get_merge_type(
+          focal_tile.get_type(),
+          other_tile.get_type()
+        );
+        //focal tile becomes merged type
+        focal_tile.set_type(merged_type);
+        //other tile is swapped to the back, then deleted
+        m_tiles[j] = m_tiles.back();
+        m_tiles.pop_back();
+        //change the selected tile
+        m_selected.clear();
+        assert(m_selected.empty());
+        return; //!OCLINT early return the only good option?
+      }
     }
   }
 }
