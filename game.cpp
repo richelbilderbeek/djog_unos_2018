da56b1527f1d853f939e8350c6165cab0a8d1c73
RafayelGardishyan <rgardishyan@gmail.com>
2019-01-24 20:51:17 +0100

    i think i've fixed issue nr.314

--- game.cpp
+++ game.cpp
@@ -3,12 +3,14 @@
 #include "game.h"
 #include "tile_id.h"
 #include "sfml_resources.h" //NOTE Until we have hitboxes
+#include "sfml_camera.h"
 #include <cassert>
 #include <iostream>
 #include <fstream>
 #include <cstdio>
 #include <QFile>
 #include <algorithm>
+
 
 game::game(const std::vector<tile>& tiles,
            const std::vector<agent>& agents,
@@ -51,6 +53,43 @@ void game::process_events()
   }
 
   ++m_n_tick;
+}
+
+void game::move_tiles(sf::RenderWindow& window, sfml_camera& camera){
+  bool clicked_tile = false;
+  for (unsigned i = 0; i < m_tiles.size(); i++)
+  {
+    if (m_tiles.at(i).tile_contains(
+          sf::Mouse::getPosition(window).x + camera.x,
+          sf::Mouse::getPosition(window).y + camera.y))
+    {
+      bool moving = false;
+      for (unsigned j = 0; j < m_tiles.size(); j++)
+      {
+        if (m_tiles.at(j).get_dx() != 0 || m_tiles.at(j).get_dy() != 0){
+          moving = true;
+        }
+      }
+      if (!moving){
+        tile s_tile = m_tiles.at(i);
+        m_selected.clear();
+        m_selected.push_back(s_tile.get_id());
+        clicked_tile = true;
+      }
+    }
+  }
+  if (clicked_tile == false)
+  {
+    bool moving = false;
+    for (unsigned i = 0; i < m_tiles.size(); i++)
+    {
+      if (m_tiles.at(i).get_dx() != 0 || m_tiles.at(i).get_dy() != 0){
+        moving = true;
+      }
+    }
+    if (!moving)
+      m_selected.clear();
+  }
 }
 
 void game::merge_tiles() { //!OCLINT must simplify
