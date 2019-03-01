c649b476855ec889cdbe2fa494b4eb0f5ed5255c
Rob Krüger <4958@praedinius.nl>
2019-03-01 21:12:11 +0100

    oclint

--- agent.cpp
+++ agent.cpp
@@ -63,7 +63,7 @@ std::vector<agent_type> can_eat(const agent_type type) {
 }
 
 bool is_plant(const agent_type type) {
-    if (type == agent_type::plankton || type == agent_type::grass || type == agent_type::tree
+    if (type == agent_type::plankton || type == agent_type::grass || type == agent_type::tree //!OCLINT TODO: redundant if statement
         || type == agent_type::foxgloves) //!OCLINT TODO: redundant if statement
     {
         return true;
