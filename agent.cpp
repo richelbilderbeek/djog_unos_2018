93a3abe71046e4da4cadbe7926c8079039b0b035
Enzo9 <46609181+Enzo9@users.noreply.github.com>
2019-03-08 20:37:15 +0100

    fixed merge conflict again

--- agent.cpp
+++ agent.cpp
@@ -64,31 +64,14 @@ std::vector<agent_type> can_eat(const agent_type type) {
   }
 }
 
-<<<<<<< HEAD
-bool is_plant(const agent_type type) {
-    if (type == agent_type::sunflower ||
-        type == agent_type::grass ||
-        type == agent_type::tree) //!OCLINT TODO: redundant if statement
-    {
-        return true;
-    }
-    else
-    {   //!OCLINT TODO: unnecessary else statement
-        return false;
-    }
-=======
-bool is_plant(const agent_type type) noexcept
-{
-  const std::set<agent_type> plants = {
-    agent_type::plankton, //Some plankton are also bacteria, archea, protozoa or animals
-    agent_type::grass,
-    agent_type::tree,
-    agent_type::cactus,
-    agent_type::foxgloves,
-    agent_type::venus_fly_trap
-  };
-  return plants.count(type);
->>>>>>> develop
+bool is_plant(const agent_type type) noexcept {
+  //Some plankton are also bacteria, archea, protozoa or animals
+  return type == agent_type::plankton ||
+         type == agent_type::grass ||
+         type == agent_type::tree ||
+         type == agent_type::cactus ||
+         type == agent_type::foxgloves ||
+         type == agent_type::venus_fly_trap;
 }
 
 void agent::eat(const game& g) {
@@ -324,11 +307,8 @@ std::vector<agent> create_default_agents() noexcept //!OCLINT indeed too long
     agent a3(agent_type::grass, 70, 40, 50 + std::rand() / (RAND_MAX / (100 - 50 + 1) + 1));
     move_agent_to_tile(a3, 0, 0);
     agents.push_back(a3);
-<<<<<<< HEAD
     agent a4(agent_type::sunflower, 42, 112);
-=======
-    agent a4(agent_type::foxgloves, 60, 70);
->>>>>>> develop
+    agent a5(agent_type::foxgloves, 60, 70);
     move_agent_to_tile(a4, 0, 0);
     agents.push_back(a4);
   }
