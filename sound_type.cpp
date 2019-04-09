d8c27ae3d947e10fe348a49db0ad3c22b05b18e5
janderkkotlarski <j.d.kotlarski@gmx.com>
2019-04-10 00:11:33 +0200

    added a few lines

--- sound_type.cpp
+++ sound_type.cpp
@@ -1,5 +1,6 @@
 #include "sound_type.h"
 
+#include <string>
 #include <algorithm>
 #include <cassert>
 
@@ -11,6 +12,21 @@ std::vector<sound_type> get_all_sound_types()
     sound_type::tile_rotate,
     sound_type::tile_collision
   };
+}
+
+std::string to_string(const sound_type& st)
+{
+  switch (st)
+  {
+    case sound_type::tile_move:
+      return "tile_move";
+    case sound_type::tile_rotate:
+      return "tile_rotate";
+    case sound_type::tile_collision:
+      return "tile_collision";
+    default:
+      return "no_sound";
+  }
 }
 
 void test_sound_type()
--- sound_type.h
+++ sound_type.h
@@ -1,6 +1,7 @@
 #ifndef SOUND_TYPE_H
 #define SOUND_TYPE_H
 
+#include <iosfwd>
 #include <vector>
 
 enum class sound_type
@@ -10,8 +11,13 @@ enum class sound_type
   tile_collision
 };
 
+/// Get all sound types
 std::vector<sound_type> get_all_sound_types();
 
+/// Convert
+std::string to_string(const sound_type& st);
+
+/// Test the sound_type class
 void test_sound_type();
 
 #endif // SOUND_TYPE_H
