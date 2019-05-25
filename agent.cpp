b929ec212b22fa411ca1779d94cd71be4289cb73
Rob Krüger <4958@praedinius.nl>
2019-05-25 22:33:36 +0200

    forgot somethign

--- agent.cpp
+++ agent.cpp
@@ -478,162 +478,154 @@ void agent::damage_own_type(game &g, agent_type type)
 std::vector<agent> create_default_agents() noexcept //!OCLINT indeed too long
 {
   std::vector<agent> agents;
-//  {
-//    agent a1(agent_type::chameleon, 0, 0, 1, 0, can_eat(agent_type::chameleon));
-//    move_agent_to_tile(a1, -3, 0);
-//    agents.push_back(a1);
-//  }
-//  {
-//    agent a1(agent_type::cow, 0, 0, 1, 0, can_eat(agent_type::cow));
-//    move_agent_to_tile(a1, 0, 0);
-//    agents.push_back(a1);
-//    agent a2(agent_type::cow, 40, 70, 1, 0, can_eat(agent_type::cow));
-//    move_agent_to_tile(a2, 0, 0);
-//    agents.push_back(a2);
-//    agent a3(agent_type::grass, 70, 40, 50 + random_double(0, 50)
-//             , 0, can_eat(agent_type::grass));
-//    move_agent_to_tile(a3, 0, 0);
-//    agents.push_back(a3);
-//    agent a4(agent_type::sunflower, 42, 112, 1, 0, can_eat(agent_type::sunflower));
-//    agent a5(agent_type::foxgloves, 60, 70, 1, 0, can_eat(agent_type::foxgloves));
-//    move_agent_to_tile(a4, 0, 0);
-//    agents.push_back(a4);
-//    move_agent_to_tile(a5, 0, 0);
-//    agents.push_back(a5);
-//  }
-//  {
-//    agent a1(agent_type::cow, 0, 0, 1, 0, can_eat(agent_type::cow));
-//    move_agent_to_tile(a1, 1, 0);
-//    agents.push_back(a1);
-//    agent a2(agent_type::cow, 90, 30, 1, 0, can_eat(agent_type::cow));
-//    move_agent_to_tile(a2, 1, 0);
-//    agents.push_back(a2);
-//    agent a3(agent_type::cow, 30, 90, 1, 0, can_eat(agent_type::cow));
-//    move_agent_to_tile(a3, 1, 0);
-//    agents.push_back(a3);
-//    agent a4(agent_type::worm, 50, 130, 1, 0, can_eat(agent_type::worm));
-//    move_agent_to_tile(a4, 1, 0);
-//    agents.push_back(a4);
-//  }
-//  {
-//    agent a1(agent_type::crocodile, 30, 160, 1, 0, can_eat(agent_type::crocodile));
-//    move_agent_to_tile(a1, 0, 2);
-//    agents.push_back(a1);
-//    agent a2(agent_type::snake, 50, 15, 1, 0, can_eat(agent_type::snake));
-//    move_agent_to_tile(a2, 0, 2);
-//    agents.push_back(a2);
-//    agent a3(agent_type::venus_fly_trap, 30, 160, 1000, 0,
-//             can_eat(agent_type::venus_fly_trap));
-//    move_agent_to_tile(a3, 0, 2);
-//    agents.push_back(a3);
-//    agent a4(agent_type::cactus, 10, 15, 100, 0, can_eat(agent_type::cactus));
-//    move_agent_to_tile(a4, 0, 2);
-//    agents.push_back(a4);
-//    agent a5(agent_type::cactus, 30, 120, 100, 0, can_eat(agent_type::cactus));
-//    move_agent_to_tile(a5, 0, 2);
-//    agents.push_back(a5);
-//  }
-//  {
-//    agent a1(agent_type::crocodile, 0, 0, 1, 0, can_eat(agent_type::crocodile));
-//    move_agent_to_tile(a1, 2, 1);
-//    agents.push_back(a1);
-//    agent a2(agent_type::grass, 0, 0, 50 + random_double(0, 50),
-//             0, can_eat(agent_type::grass));
-//    move_agent_to_tile(a2, 2, 1);
-//    agents.push_back(a2);
-//  }
-//  {
-//    agent a1(agent_type::fish, 0, 0, 1, 0, can_eat(agent_type::fish));
-//    move_agent_to_tile(a1, 3, 2);
-//    agents.push_back(a1);
-//    agent a2(agent_type::fish, 10, 10, 1, 0, can_eat(agent_type::fish));
-//    move_agent_to_tile(a2, 3, 2);
-//    agents.push_back(a2);
-//    agent a3(agent_type::octopus, 50, 70, 1, 0, can_eat(agent_type::octopus));
-//    move_agent_to_tile(a3, 3, 2);
-//    agents.push_back(a3);
-//  }
-//  {
-//    agent a1(agent_type::whale, 0, 0, 1, 0, can_eat(agent_type::whale));
-//    move_agent_to_tile(a1, 3, 2);
-//    agents.push_back(a1);
-//  }
-//  {
-//    agent a1(agent_type::whale, 0, 0, 1, 0, can_eat(agent_type::whale));
-//    move_agent_to_tile(a1, 3, 2);
-//    agents.push_back(a1);
-//  }
-//  {
-//    agent a1(agent_type::fish, 0, 0, 1, 0, can_eat(agent_type::fish));
-//    move_agent_to_tile(a1, 4, 2);
-//    agents.push_back(a1);
-//    agent a2(agent_type::fish, 10, 10, 1, 0, can_eat(agent_type::fish));
-//    move_agent_to_tile(a2, 4, 2);
-//    agents.push_back(a2);
-//  }
-//  {
-//    agent a1(agent_type::grass, 0, 0, 50 + random_double(0, 50),
-//             0, can_eat(agent_type::grass));
-//    move_agent_to_tile(a1, 1, -1);
-//    agents.push_back(a1);
-//    agent a2(agent_type::giraffe, 10, 20, 1, 0, can_eat(agent_type::giraffe));
-//    move_agent_to_tile(a2, 1, -1);
-//    agents.push_back(a2);
-//    //agent a3(agent_type::lion, 120, 20);
-//    //move_agent_to_tile(a3, 1, -1);
-//    //agents.push_back(a3);
-//    agent a4(agent_type::crocodile, 180, 20, 1, 0, can_eat(agent_type::crocodile));
-//    move_agent_to_tile(a4, 1, -1);
-//    agents.push_back(a4);
-//  }
-//  {
-//    agent a1(agent_type::tree, 10, 20, 1, 0, can_eat(agent_type::tree));
-//    move_agent_to_tile(a1, 4, -1);
-//    agents.push_back(a1);
-//    agent a2(agent_type::tree, 40, 10, 1, 0, can_eat(agent_type::tree));
-//    move_agent_to_tile(a2, 4, -1);
-//    agents.push_back(a2);
-//    agent a3(agent_type::tree, 50, 35, 1, 0, can_eat(agent_type::tree));
-//    move_agent_to_tile(a3, 4, -1);
-//    agents.push_back(a3);
-//    agent a4(agent_type::tree, 60, 40, 1, 0, can_eat(agent_type::tree));
-//    move_agent_to_tile(a4, 4, -1);
-//    agents.push_back(a4);
-//    agent a5(agent_type::tree, 35, 65, 1, 0, can_eat(agent_type::tree));
-//    move_agent_to_tile(a5, 4, -1);
-//    agents.push_back(a5);
-//    agent a6(agent_type::tree, 0, 0, 1, 0, can_eat(agent_type::tree));
-//    move_agent_to_tile(a6, 4, -1);
-//    agents.push_back(a6);
-//    agent a7(agent_type::spider, 40, 40, 1, 0, can_eat(agent_type::spider));
-//    move_agent_to_tile(a7, 4, -1);
-//    agents.push_back(a7);
-//    agent a8(agent_type::bird, 75, 150, 1, 0, can_eat(agent_type::bird));
-//    move_agent_to_tile(a8, 4, -1);
-//    agents.push_back(a8);
-//  }
-//  {
-//    agent a1(agent_type::tree, 90, 170, 1, 0, can_eat(agent_type::tree));
-//    move_agent_to_tile(a1, 0, -2);
-//    agents.push_back(a1);
-//    agent a2(agent_type::squirrel, 90, 150, 1, 0, can_eat(agent_type::squirrel));
-//    move_agent_to_tile(a2, 0, -2);
-//    agents.push_back(a2);
-//  }
-//  {
-//    agent a1(agent_type::goat, 190, 90, 1, 0, can_eat(agent_type::goat));
-//    move_agent_to_tile(a1, 1, 2);
-//    agents.push_back(a1);
-//    agent a2(agent_type::goat, 50, 80, 1, 0, can_eat(agent_type::goat));
-//    move_agent_to_tile(a2, 1, 2);
-//    agents.push_back(a2);
-//  }
   {
-    agent a1(agent_type::cow, 190, 90, 1, 0, can_eat(agent_type::cow));
-    move_agent_to_tile(a1, -1, 0);
+    agent a1(agent_type::chameleon, 0, 0, 1, 0, can_eat(agent_type::chameleon));
+    move_agent_to_tile(a1, -3, 0);
     agents.push_back(a1);
-    agent a2(agent_type::grass, 50, 80, 1, 0, can_eat(agent_type::grass));
-    move_agent_to_tile(a2, 2, 0);
+  }
+  {
+    agent a1(agent_type::cow, 0, 0, 1, 0, can_eat(agent_type::cow));
+    move_agent_to_tile(a1, 0, 0);
+    agents.push_back(a1);
+    agent a2(agent_type::cow, 40, 70, 1, 0, can_eat(agent_type::cow));
+    move_agent_to_tile(a2, 0, 0);
+    agents.push_back(a2);
+    agent a3(agent_type::grass, 70, 40, 50 + random_double(0, 50)
+             , 0, can_eat(agent_type::grass));
+    move_agent_to_tile(a3, 0, 0);
+    agents.push_back(a3);
+    agent a4(agent_type::sunflower, 42, 112, 1, 0, can_eat(agent_type::sunflower));
+    agent a5(agent_type::foxgloves, 60, 70, 1, 0, can_eat(agent_type::foxgloves));
+    move_agent_to_tile(a4, 0, 0);
+    agents.push_back(a4);
+    move_agent_to_tile(a5, 0, 0);
+    agents.push_back(a5);
+  }
+  {
+    agent a1(agent_type::cow, 0, 0, 1, 0, can_eat(agent_type::cow));
+    move_agent_to_tile(a1, 1, 0);
+    agents.push_back(a1);
+    agent a2(agent_type::cow, 90, 30, 1, 0, can_eat(agent_type::cow));
+    move_agent_to_tile(a2, 1, 0);
+    agents.push_back(a2);
+    agent a3(agent_type::cow, 30, 90, 1, 0, can_eat(agent_type::cow));
+    move_agent_to_tile(a3, 1, 0);
+    agents.push_back(a3);
+    agent a4(agent_type::worm, 50, 130, 1, 0, can_eat(agent_type::worm));
+    move_agent_to_tile(a4, 1, 0);
+    agents.push_back(a4);
+  }
+  {
+    agent a1(agent_type::crocodile, 30, 160, 1, 0, can_eat(agent_type::crocodile));
+    move_agent_to_tile(a1, 0, 2);
+    agents.push_back(a1);
+    agent a2(agent_type::snake, 50, 15, 1, 0, can_eat(agent_type::snake));
+    move_agent_to_tile(a2, 0, 2);
+    agents.push_back(a2);
+    agent a3(agent_type::venus_fly_trap, 30, 160, 1000, 0,
+             can_eat(agent_type::venus_fly_trap));
+    move_agent_to_tile(a3, 0, 2);
+    agents.push_back(a3);
+    agent a4(agent_type::cactus, 10, 15, 100, 0, can_eat(agent_type::cactus));
+    move_agent_to_tile(a4, 0, 2);
+    agents.push_back(a4);
+    agent a5(agent_type::cactus, 30, 120, 100, 0, can_eat(agent_type::cactus));
+    move_agent_to_tile(a5, 0, 2);
+    agents.push_back(a5);
+  }
+  {
+    agent a1(agent_type::crocodile, 0, 0, 1, 0, can_eat(agent_type::crocodile));
+    move_agent_to_tile(a1, 2, 1);
+    agents.push_back(a1);
+    agent a2(agent_type::grass, 0, 0, 50 + random_double(0, 50),
+             0, can_eat(agent_type::grass));
+    move_agent_to_tile(a2, 2, 1);
+    agents.push_back(a2);
+  }
+  {
+    agent a1(agent_type::fish, 0, 0, 1, 0, can_eat(agent_type::fish));
+    move_agent_to_tile(a1, 3, 2);
+    agents.push_back(a1);
+    agent a2(agent_type::fish, 10, 10, 1, 0, can_eat(agent_type::fish));
+    move_agent_to_tile(a2, 3, 2);
+    agents.push_back(a2);
+    agent a3(agent_type::octopus, 50, 70, 1, 0, can_eat(agent_type::octopus));
+    move_agent_to_tile(a3, 3, 2);
+    agents.push_back(a3);
+  }
+  {
+    agent a1(agent_type::whale, 0, 0, 1, 0, can_eat(agent_type::whale));
+    move_agent_to_tile(a1, 3, 2);
+    agents.push_back(a1);
+  }
+  {
+    agent a1(agent_type::whale, 0, 0, 1, 0, can_eat(agent_type::whale));
+    move_agent_to_tile(a1, 3, 2);
+    agents.push_back(a1);
+  }
+  {
+    agent a1(agent_type::fish, 0, 0, 1, 0, can_eat(agent_type::fish));
+    move_agent_to_tile(a1, 4, 2);
+    agents.push_back(a1);
+    agent a2(agent_type::fish, 10, 10, 1, 0, can_eat(agent_type::fish));
+    move_agent_to_tile(a2, 4, 2);
+    agents.push_back(a2);
+  }
+  {
+    agent a1(agent_type::grass, 0, 0, 50 + random_double(0, 50),
+             0, can_eat(agent_type::grass));
+    move_agent_to_tile(a1, 1, -1);
+    agents.push_back(a1);
+    agent a2(agent_type::giraffe, 10, 20, 1, 0, can_eat(agent_type::giraffe));
+    move_agent_to_tile(a2, 1, -1);
+    agents.push_back(a2);
+    //agent a3(agent_type::lion, 120, 20);
+    //move_agent_to_tile(a3, 1, -1);
+    //agents.push_back(a3);
+    agent a4(agent_type::crocodile, 180, 20, 1, 0, can_eat(agent_type::crocodile));
+    move_agent_to_tile(a4, 1, -1);
+    agents.push_back(a4);
+  }
+  {
+    agent a1(agent_type::tree, 10, 20, 1, 0, can_eat(agent_type::tree));
+    move_agent_to_tile(a1, 4, -1);
+    agents.push_back(a1);
+    agent a2(agent_type::tree, 40, 10, 1, 0, can_eat(agent_type::tree));
+    move_agent_to_tile(a2, 4, -1);
+    agents.push_back(a2);
+    agent a3(agent_type::tree, 50, 35, 1, 0, can_eat(agent_type::tree));
+    move_agent_to_tile(a3, 4, -1);
+    agents.push_back(a3);
+    agent a4(agent_type::tree, 60, 40, 1, 0, can_eat(agent_type::tree));
+    move_agent_to_tile(a4, 4, -1);
+    agents.push_back(a4);
+    agent a5(agent_type::tree, 35, 65, 1, 0, can_eat(agent_type::tree));
+    move_agent_to_tile(a5, 4, -1);
+    agents.push_back(a5);
+    agent a6(agent_type::tree, 0, 0, 1, 0, can_eat(agent_type::tree));
+    move_agent_to_tile(a6, 4, -1);
+    agents.push_back(a6);
+    agent a7(agent_type::spider, 40, 40, 1, 0, can_eat(agent_type::spider));
+    move_agent_to_tile(a7, 4, -1);
+    agents.push_back(a7);
+    agent a8(agent_type::bird, 75, 150, 1, 0, can_eat(agent_type::bird));
+    move_agent_to_tile(a8, 4, -1);
+    agents.push_back(a8);
+  }
+  {
+    agent a1(agent_type::tree, 90, 170, 1, 0, can_eat(agent_type::tree));
+    move_agent_to_tile(a1, 0, -2);
+    agents.push_back(a1);
+    agent a2(agent_type::squirrel, 90, 150, 1, 0, can_eat(agent_type::squirrel));
+    move_agent_to_tile(a2, 0, -2);
+    agents.push_back(a2);
+  }
+  {
+    agent a1(agent_type::goat, 190, 90, 1, 0, can_eat(agent_type::goat));
+    move_agent_to_tile(a1, 1, 2);
+    agents.push_back(a1);
+    agent a2(agent_type::goat, 50, 80, 1, 0, can_eat(agent_type::goat));
+    move_agent_to_tile(a2, 1, 2);
     agents.push_back(a2);
   }
   return agents;
